49bfceead479acb3a57a6a91cc66c3dd0747a73b
Ong, Seth <seth.ong24@imperial.ac.uk>
2026-01-15 16:21:10 +0000

    fix frontend linting

---
+++ frontend/eslint.config.js
@@ -0,0 +1,45 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  // Ignore generated output + deps
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+
+  // Lint source files only
+  {
+    files: ['src/**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.browser,
+        describe: 'readonly',
+        it: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+        vi: 'readonly',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': 'off',
+      'no-unused-vars': 'off',
+    },
+  },
+]
